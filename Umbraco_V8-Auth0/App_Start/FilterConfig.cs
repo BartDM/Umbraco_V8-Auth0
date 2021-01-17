8ece5f00d26e9b3edf15667ef1ebe77311be751d
Bart De Meyer <bdemeyer@usgpeople.be>
2021-01-17 16:05:49 +0100

    Base web project

---
+++ Umbraco_V8-Auth0/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Umnraco_V8_Auth0
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Umbraco_V8-Auth0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Umnraco_V8_Auth0.MvcApplication" Language="C#" %>
